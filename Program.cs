98e23e04d0e5ebd45b7d6a39ad1a2b646b59d2ac
Amadeusz Wieczorek <amwieczo@microsoft.com>
2019-08-27 16:25:31 -0700

    wip

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RainbowTableSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alphabetLength = 25;
+            int indexOfA = 97;
+            List<char> alphabet = new List<char>(alphabetLength);
+            for (int i = indexOfA; i < indexOfA + alphabetLength; i++)
+            {
+                var letter = (char)i;
+                Console.WriteLine(letter);
+                alphabet.Add(letter);
+            }
+            Console.ReadLine();
+        }
+    }
+}
