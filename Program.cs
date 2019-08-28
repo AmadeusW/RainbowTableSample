0c4538c81cdeca09f6ca2e3901502fc78c831c80
Amadeusz Wieczorek <amwieczo@microsoft.com>
2019-08-28 13:50:39 -0700

    permutes alphabet to generate words

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace RainbowTableSample
 {
@@ -16,7 +19,53 @@ namespace RainbowTableSample
                 Console.WriteLine(letter);
                 alphabet.Add(letter);
             }
+
+            int wordLength = 6;
+            int p = 0;
+            int maxPermutations = (int)Math.Pow(alphabetLength, wordLength);
+            char[] word = new char[wordLength];
+
+            var start = DateTime.Now;
+
+            var sha = SHA256.Create();
+            int counter = 0;
+            var q = from c1 in alphabet
+                    from c2 in alphabet
+                    from c3 in alphabet
+                    from c4 in alphabet
+                    from c5 in alphabet
+                    from c6 in alphabet
+                    select new char[]
+                      { c1, c2, c3, c4, c5, c6 };
+            foreach (var w in q)
+            {
+                var bytes = Encoding.UTF8.GetBytes(w);
+                var hash = sha.ComputeHash(bytes);
+                if (counter % 1000 == 0)
+                    Console.WriteLine($"{counter}/{maxPermutations}");
+                counter++;
+            }
+
+            var stop = DateTime.Now;
+            Console.WriteLine((stop - start).TotalSeconds);
+            /*
+            do
+            {
+                for (int letter = 0; letter < wordLength; letter++)
+                {
+                    var letterCeiling = Math.Pow(alphabetLength, letter);
+                    var c = (int)(p / letterCeiling);
+                    var r = p - letterCeiling;
+                    var c2 = p % Math.Pow(alphabetLength, letter);
+                    word[letter] = (char)(c + indexOfA);
+                }
+                Console.WriteLine(word);
+            }
+            while (p++ < maxPermutations);
+            */
             Console.ReadLine();
         }
+
+
     }
 }
